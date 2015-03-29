851285ce0d9a7009de5891866786d3e11a1d1de5
Michael Arnold <github@razorsedge.org>
2015-03-28 23:44:51 -0700

    first commit

---
+++ manifests/params.pp
@@ -0,0 +1,18 @@
+# == Class: openvmtools::params
+#
+# This class handles OS-specific configuration of the openvmtools module.  It
+# looks for variables in top scope (probably from an ENC such as Dashboard).  If
+# the variable doesn't exist in top scope, it falls back to a hard coded default
+# value.
+#
+# === Authors:
+#
+# Mike Arnold <mike@razorsedge.org>
+#
+# === Copyright:
+#
+# Copyright (C) 2014 Mike Arnold, unless otherwise noted.
+#
+class openvmtools::params {
+
+}
