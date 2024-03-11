5f1d0c21f2cfff381a6ac307599f25a001e06478
Roberto Valentini <roberto.valentini89@gmail.com>
2024-03-11 13:47:57 +0100

    Remove Debian 7,8,9 and 10 from supported OS

--- spec/functions/supported_spec.rb
+++ spec/functions/supported_spec.rb
@@ -15,6 +15,23 @@ describe 'openvmtools::supported' do
       }
     end
 
+    it 'returns false' do
+      is_expected.to run.with_params('openvmtools').and_return(false)
+    end
+  end
+
+  context 'On Debian 11' do
+    let(:facts) do
+      {
+        os: {
+          name: 'Debian',
+          release: {
+            major: '11',
+          }
+        }
+      }
+    end
+
     it 'returns true' do
       is_expected.to run.with_params('openvmtools').and_return(true)
     end
